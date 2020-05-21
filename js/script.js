4037caf2ad7cd42566f16051fc72af8af2045378
Abbassihraf <hraf.abbassi@gmail.com>
2020-05-21 14:25:55 +0000

    delete 3 section

--- js/script.js
+++ js/script.js
@@ -20,3 +20,39 @@ const navSlide = () => {
 
 }
 navSlide();
+
+
+
+
+
+
+
+// clients-carousel
+if ($('.clients-carousel').length) {
+    $('.clients-carousel').owlCarousel({
+        loop:true,
+        margin:30,
+        nav:false,
+        smartSpeed: 3000,
+        autoplay: true,
+        navText: [ '<span class="fa fa-angle-left"></span>', '<span class="fa fa-angle-right"></span>' ],
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:2
+            },
+            600:{
+                items:3
+            },
+            800:{
+                items:4
+            },			
+            1200:{
+                items:4
+            }
+
+        }
+    });    		
+}
